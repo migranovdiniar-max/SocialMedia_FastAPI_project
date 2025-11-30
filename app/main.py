66ee5d8991829a561a96f4e629d79e9e9a6499cb
Dinyar <migranovdiniar@gmail.com>
2025-11-30 03:44:04 +0300

    added authentication

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import time
 from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -53,6 +53,7 @@ def find_index_post(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
--- app/schemas.py
+++ app/schemas.py
@@ -34,3 +34,8 @@ class UserOut(BaseModel):
     model_config = {
     "from_attributes": True
     }
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
